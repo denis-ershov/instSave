62cc350fe72a26d2c4732ea9155ab5f4dde99f00
Denis Ershov <W1ns-Smile@yandex.ru>
2022-04-05 16:10:49 +0300

    Update

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-var url = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/fA9uwTtkSN/";
+function delay() {
+   return new Promise((resolve) => setTimeout(resolve, 6000));
+ }
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+async function inst() {
+   await delay();
+   let post = {};
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+   for (key in data.saved_saved_media) {
+      var loc = data.saved_saved_media[key].string_map_data.base.href;
+      var id = loc.split('/').slice(-2)[0];
+      //console.log(id);
+      let opt = {
+         method: 'GET',
+         headers: {
+            'X-RapidAPI-Host': 'instagram-bulk-profile-scrapper.p.rapidapi.com',
+            'X-RapidAPI-Key': 'baf3e80aa8mshfe8d689373c0219p11bb09jsn8e00ad473782'
+         }
+      };
+   
+     let url = "https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/media_by_id?shortcode=" + id + "&response_type=feeds";
+     let response = await fetch(url, opt);
+     let ibase = await response.json();
+     //console.log(ibase[0].items[0].caption.text);
+     post[key] = {
+        img: ibase[0].items[0].image_versions2.candidates[2].url,
+        text: ibase[0].items[0].caption.text
+     }
+     console.log(post[key]);
+     console.log('Обработано ' + key);
+   }
+   console.log(post);
+}
 
-xhr.send();
+inst();
